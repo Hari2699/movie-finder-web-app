a92278f4ec1f58542b4c661cb28ef4ca7a9e1908
Sandy Solaqa <ssolaqa8226@sdsu.edu>
2023-10-23 19:19:18 -0700

    Feature Page-#7: updated signin page with code

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import "./App.css";
 
 import Home from "./pages/home/Home";
 import SignUp from './pages/signup/SignUp';
+import SignIn from './pages/signin/SignIn';
 
 function App() {
   
   return (
+    //Header needed here
     <Router>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
           </Switch>
     </Router>
+    //Footer needed her
   );
 }
 
