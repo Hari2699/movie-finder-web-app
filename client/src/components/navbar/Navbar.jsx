2e78fe954e5e7b0fc7973ec8bf64b8edff6ed036
Hariharan Baskar <hbaskar8622@LIB-CHK-E054709.local>
2023-10-24 19:02:44 -0700

    Feature Component #6 : updated the navbar component

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import classes from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import { useAuth } from '../../context/auth/authState'
+import Notification from './../../services/NotificationService';
 
-const Navbar = () => {
+const NavBtn = ({ classname, to, text }) => {
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className={ classes.navlinks }>
+      <Link className={ classname } to={ to }>
+        { text }
+      </Link>
     </div>
   )
 }
 
-export default Navbar
+const Navbar = () => {
+  const { logout, token } = useAuth()
+
+  const logoutHandler = () => {
+    logout()
+    Notification.show({
+      message: 'Successfully logged out',
+      status: true
+    })
+  }
+
+  let authLinks = (
+    <>
+      <NavBtn classname={ classes.navbtnlink1 } to='/profile' text='Profile' />
+      <button className={ classes.navbtnlink2 } onClick={ logoutHandler }>Logout</button>
+    </>
+  )
+
+  let guestLinks = (
+    <>
+      <NavBtn classname={ classes.navbtnlink1 } to='/signup' text='Sign Up' />
+      <NavBtn classname={ classes.navbtnlink2 } to='/signin' text='Sign In' />
+    </>
+  )
+
+  return (
+    <div className={ classes.navbarBg }>
+      <div className={ classes.navcontainer }>
+        <div className={ classes.logo }>
+          <Link to={ "/" }>
+            <div className={ classes.navlogo } />
+          </Link>
+        </div>
+        <div className={ classes.navmenu }>
+          <div className={ classes.navitem }>
+            <div className={ classes.navbtn }>
+              {
+                token ? authLinks : guestLinks
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
