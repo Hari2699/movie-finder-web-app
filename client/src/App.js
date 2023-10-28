dee0e624992c452e01f79bb48574486946a44e8e
Alan Linan <32002122+Alinan94@users.noreply.github.com>
2023-10-28 11:03:34 -0700

    Updated Movie cards to facilitate movie row addition

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
-import axios from 'axios';
-import { MovieCard } from "./components/MovieCard/MovieCard";
 
 import Home from "./pages/home/Home";
+import SignUp from './pages/signup/SignUp';
+import SignIn from './pages/signin/SignIn';
 
 function App() {
   
-  const [movies, setMovies] = useState([]);
-
-    const getmovie = async () =>{
-         try{
-            const {data} = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=7af1cba5c48ce69c437e486757fef8f7")
-            setMovies(data.results);
-         }catch(err){
-            console.log(movies);
-         }
-       }
-       useEffect(()=>{
-        getmovie()
-       },[])
-
   return (
-    <div className='App'>  
-      <Router>
+    <Router>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
           </Switch>
-      </Router>
-
-      <main className='main'>
-        {
-          movies && movies.length > 0 && movies.map(movie => <MovieCard key ={movie.id} movie={movie}/>)
-        }
-      </main>
-    </div>
+    </Router>
   );
 }
 
--- client/src/components/movie-card/MovieCard.jsx
+++ client/src/components/movie-card/MovieCard.jsx
@@ -2,22 +2,11 @@ import './MovieCard.module.css';
 
 export const MovieCard = ({movie}) =>{
 
-    const { title, poster_path, overview, vote_average} = movie;
+    const {poster_path} = movie;
 
     return(
         <div className="card-container">
-            <div className="card-img-container">
                 <img src= {`https://image.tmdb.org/t/p/w500${poster_path}`} alt="movie-card" />
-            </div>
-            <div className="metrics">
-                <h1 className="title">{title}</h1>           
-                <p className="summary">{overview}</p><br />
-            <div/>
-            <div className='details'>
-                <span className="genre">Genre</span>
-                <span className="rating">{`Rating:${vote_average}`}</span>
-            </div>
-            </div>
         </div>
     )
 }
