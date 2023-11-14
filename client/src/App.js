a2203afedb19c735ef2ffb0b34984dfecb016a34
Hariharan Baskar <hbaskar8622@LIB-CHK-E054709.local>
2023-11-13 23:15:00 -0800

    Feature Page #3 : Updated routes with Guest and Protected Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import "./App.css";
 
 import Home from "./pages/home/Home";
 import SignUp from './pages/signup/SignUp';
 import SignIn from './pages/signin/SignIn';
+import GotoPage from './pages/gotoPage/GoToPage';
+import MoviePage from './pages/movies-page/MoviesPage';
+import Profile from './pages/profile/Profile';
+import Notification from './services/NotificationService';
+import ForgetPassword from './pages/forget-password/ForgetPassword';
+import AuthProvider from './context/auth/authState';
+import MovieProvider from './context/movies/movieState';
+import GuestRoute from './routes/GuestRoute';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
   
   return (
     <Router>
+      <Notification />
+      <AuthProvider>
+        <MovieProvider>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/signin" component={SignIn} />
+            <GuestRoute path="/signin" component={SignIn} />
+            <GuestRoute path="/signup" component={ SignUp } />
+            <GuestRoute path="/forgot-password" component={ForgetPassword} />
+            <ProtectedRoute path="/profile" component={ Profile } />
+            <Route path="/movie/:id" component={MoviePage} />
+            <Route path="/mostpopular" component={ GotoPage } />
+            <Route path="/latestmovies" component={ GotoPage } />
+            <Route path="/toprated" component={ GotoPage } />
+            <Redirect to="/"></Redirect>
           </Switch>
+        </MovieProvider>
+      </AuthProvider>
     </Router>
   );
 }
