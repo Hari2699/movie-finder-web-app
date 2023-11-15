1b757b1b38772f07cae001a345557c828e1ca0b2
Hariharan Baskar <hbaskar8622@LIB-CHK-E054709.local>
2023-11-14 22:02:18 -0800

    Feature Component #22 : added comma separated text

---
+++ client/src/components/comma-separated-text/CommaSeperatedText.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const CommaSeperatedText = ({ textArray }) => {
+    return (
+        <>
+            { 
+                textArray.map((text,idx) => (
+                    <span key={ idx } > 
+                        { (idx + 1) === textArray.length ? text.toUpperCase() : `${text.toUpperCase()}, ` } 
+                    </span>
+                )) 
+            }   
+        </>
+    )
+}
+
+export default CommaSeperatedText;
