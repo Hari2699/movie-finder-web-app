11bc65d2631e872ad2ee19cc0ac728f40f2e73d3
Hariharan Baskar <hbaskar8622@LIB-CHK-E054709.local>
2023-11-18 14:02:37 -0800

    Feature Component #78 : added platform images

---
+++ client/src/components/platforms-images/PlatformsImages.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './PlatformsImages.module.css';
+
+const platformsImages = ({ platforms }) => {
+    
+
+    return (
+        <>
+            { 
+                platforms.length === 0 ? (<span className={ classes.noPlatformText }> N/A </span>) :
+                platforms.map( (platform,idx) => 
+                    <div key={ idx } title={ platform.providerName } className={ classes.platformImage } >
+                        <img src={ platform.logoPath } alt={ platform.providerName }/>
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default platformsImages;
