c0b430eb275d0fbeb1fd16ec0f973d9abf0f12a8
Hariharan Baskar <hbaskar8622@LIB-CHK-E054709.local>
2023-12-06 00:30:11 -0800

    Feature Component #106 : backend CI CD

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)5po$e54k@-d+8m3+3mw&-syr6s77=!vnjz3(sdsmpwrnkm=2i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
